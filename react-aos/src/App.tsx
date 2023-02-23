cfe7436985ea2f00375f65b798ec0e1710e6b41d
voidkudo <voidkudo@gmail.com>
2023-02-23 22:02:32 +0800

    feat: add scroll listener

--- react-aos/src/App.tsx
+++ react-aos/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import "aos/dist/aos.css";
 import './App.css';
@@ -9,23 +9,38 @@ import { IconButton } from '@mui/material';
 import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 
 function App() {
+  const [isBackToTopButtonShow, setIsBackToTopButtonShow] = useState<boolean>(false);
+
   const backToTop = () => {
-    window.scrollTo({top: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const checkScroll = () => {
+    if (window.scrollY > 250) {
+      setIsBackToTopButtonShow(true);
+    } else {
+      setIsBackToTopButtonShow(false);
+    }
   };
 
   useEffect(() => {
     AOS.init();
+    window.addEventListener('scroll', checkScroll)
   }, [])
 
   return (
-    <div className='App'>
+    <div>
       <Home />
+      <div className='HorizontalLine' />
       <Section1 />
-      <div className='BackToTop'>
-        <IconButton onClick={() => backToTop()}>
-          <KeyboardDoubleArrowUpIcon/>
-        </IconButton>
-      </div>
+      <div className='HorizontalLine' />
+      {isBackToTopButtonShow ?
+        <div className='BackToTop'>
+          <IconButton onClick={() => backToTop()}>
+            <KeyboardDoubleArrowUpIcon />
+          </IconButton>
+        </div> : null
+      }
     </div>
   );
 }
