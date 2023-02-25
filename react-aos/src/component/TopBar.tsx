c80fb850ddad9711a3501b3dacb1e027af2832ec
voidkudo <voidkudo@gmail.com>
2023-02-26 03:51:08 +0800

    feat: add icons

--- react-aos/src/component/TopBar.tsx
+++ react-aos/src/component/TopBar.tsx
@@ -2,9 +2,12 @@ import { Box, createTheme, Tab, Tabs, ThemeProvider } from "@mui/material";
 import { Pages } from "../App";
 import HomeIcon from '@mui/icons-material/Home';
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function TopBar() {
   const navigate = useNavigate();
+
+  const currentPage = Pages.includes(window.location.pathname.replace('/', '')) ? window.location.pathname.replace('/', '') : 'home';
 
   const theme = createTheme({
     palette: {
@@ -17,7 +20,7 @@ export default function TopBar() {
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ position: 'fixed', width: '100vw', backgroundColor: '#2B3A55' }}>
-        <Tabs value={window.location.pathname.replace('/', '')} onChange={(event, newValue) => navigate('/' + newValue)} centered indicatorColor='primary'>
+        <Tabs value={currentPage} onChange={(event, newValue) => navigate('/' + newValue)} centered indicatorColor='primary'>
           <Tab label={<span className='Tab'>HOME</span>} value='home' icon={<HomeIcon className='Tab' />} iconPosition='start' />
           {
             Pages.map(tab =>
--- react-aos/src/page/section/HomeMain.tsx
+++ react-aos/src/page/section/HomeMain.tsx
@@ -2,43 +2,83 @@ import { useEffect, useState } from "react";
 import { Icon } from "@mui/material";
 
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import OpacityIcon from '@mui/icons-material/Opacity';
 
 export default function HomeMain() {
   const [date, setDate] = useState(new Date());
-  const [temperature, setTemperature] = useState('');
+  const [weather, setWeather] = useState();
 
   const updateTime = () => {
     setDate(new Date());
   };
 
-  const updateTemperature = () => {
-    let temp = 0;
+  const updateWeather = () => {
     fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=rhrread&lang=en')
       .then(res => res.json())
-      .then(data => {
-        data.temperature.data.forEach((record: any) => {
-          temp += record.value;
-        })
-        temp /= data.temperature.data.length;
-        setTemperature(temp.toFixed(1));
+      .then(data => setWeather(data));
+  };
+
+  const getTemperature = (weather: any) => {
+    try {
+      let temperature = 0;
+      weather.temperature.data.forEach((record: any) => {
+        temperature += record.value;
       })
-      .catch(err => {
-        console.error(err);
-        setTemperature('');
-      });
+      temperature /= weather.temperature.data.length;
+      return temperature.toFixed(1);
+    }
+    catch {
+      return undefined;
+    }
+  };
+
+  const getHumidity = (weather: any) => {
+    try {
+      let humidity = 0;
+      weather.humidity.data.forEach((record: any) => {
+        humidity += record.value;
+      })
+      humidity /= weather.humidity.data.length;
+      return humidity.toFixed(0);
+    }
+    catch {
+      return undefined;
+    }
+  };
+
+  const getWeatherContent = (weather: any) => {
+    const temperature = getTemperature(weather);
+    const humidity = getHumidity(weather);
+    if (temperature !== undefined && humidity !== undefined) {
+      return (
+        <h1>
+          <Icon>
+            <ThermostatIcon />
+          </Icon>
+          {temperature + '°C・'}
+          <Icon>
+            <OpacityIcon />
+          </Icon>
+          {humidity + '%'}
+        </h1>
+      );
+    } else {
+      return <></>;
+    }
   };
 
   useEffect(() => {
-    updateTemperature();
+    updateWeather();
     setInterval(() => updateTime(), 1000);
-    setInterval(() => updateTemperature(), 1000 * 60 * 10);
+    setInterval(() => updateWeather(), 1000 * 60 * 10);
   }, []);
 
   return (
     <div className='Top'>
       <div className='TimeAndTemp'>
         <h1>{date.toTimeString().slice(0, 8)}</h1>
-        <h1>{temperature + '°C'}</h1>
+        {getWeatherContent(weather)}
       </div>
       <div className='ScrollDown'>
         <div>Scroll Down</div>
